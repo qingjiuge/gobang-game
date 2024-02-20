0091957de438a4f1eb85baa37b746a66077b1120
SmallTeddy <zhangzeming1212@gmail.com>
2024-02-20 15:54:49 +0800

    chome: deploy

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import { fileURLToPath, URL } from 'node:url'
 // https://vitejs.dev/config/
 export default ({ mode }: any) => {
     return defineConfig({
+        base: './',
         plugins: [vue()],
         resolve: {
             alias: {
